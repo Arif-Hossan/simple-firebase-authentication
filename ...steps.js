3945532595c3605fe070c8baacb86632089209f0
Arif Hossan <arif.hossan.eu@gmail.com>
2023-07-11 22:14:12 +0600

    creating firebase setup

---
+++ ...steps.js
@@ -0,0 +1,9 @@
+/*
+1.visit console.firebase.google.com
+2.create a new firebase project
+3.visit doc(go to docs) : Build > Authentication > Web > Getting Started
+4.Register Web App > Firebase Project Home > Click Web > Give name and register
+5.Install Firebase for your project: npm install firebase
+6.Dangerous :Get firebase config and put it in firebase.init.js
+7.export app from firebase.init.js
+*/
