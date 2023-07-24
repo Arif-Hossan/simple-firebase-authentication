96b650caf92c7778a559e15136909a36afdd0e25
Arif Hossan <arif.hossan.eu@gmail.com>
2023-07-24 21:59:28 +0600

    google authentication

--- ...steps.js
+++ ...steps.js
@@ -6,4 +6,8 @@
 5.Install Firebase for your project: npm install firebase
 6.Dangerous :Get firebase config and put it in firebase.init.js
 7.export app from firebase.init.js
+8.create auth using getAuth from firebase by using app from firebase.init.js
+9.create a google auth provider. do not forget to use new
+10.go to firebase > Build > authentication > sign in method
+11.enable google sign in method
 */
