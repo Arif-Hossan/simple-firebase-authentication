4a4165384debcd691d938a2075135040b5235c8c
Arif Hossan <arif.hossan.eu@gmail.com>
2023-07-24 23:53:41 +0600

    sign out and conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
-import './App.css';
-import {GoogleAuthProvider, getAuth, signInWithPopup} from 'firebase/auth'
-import app from './firebase/firebase.init';
-const auth =getAuth(app);
+import "./App.css";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import app from "./firebase/firebase.init";
+import { useState } from "react";
+const auth = getAuth(app);
 function App() {
+  const [user, setUser] = useState({});
   const provider = new GoogleAuthProvider();
-  const handleGoogleSingIn = () =>{
-    signInWithPopup(auth,provider)
-    .then(result=>{
-      const user = result.user;
-      console.log(user);
-    }).catch(error=>{
-      console.error(error);
-    })
-  }
+  const handleGoogleSingIn = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        console.log(user);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
+      })
+      .catch(() => {
+        setUser({});
+      });
+  };
   return (
     <div className="App">
-      <button onClick={handleGoogleSingIn}>Google Sign In</button>
+      {/* toggle button /conditional css */}
+      {
+        user.email?
+        <button onClick={handleSignOut}>Sign Out</button>
+        :
+        <button onClick={handleGoogleSingIn}>Google Sign In</button>
+      }
+      {user.email && (
+        <div>
+          <h3>User Name: {user.displayName}</h3>
+          <p>Email: {user.email}</p>
+          <img src={user.photoURL} alt="" />
+        </div>
+      )}
     </div>
   );
 }
