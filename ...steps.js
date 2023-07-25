496148833a9b39d0559090bc01613a0dccee5a84
Arif Hossan <arif.hossan.eu@gmail.com>
2023-07-25 23:56:01 +0600

    github authentication added

--- ...steps.js
+++ ...steps.js
@@ -6,8 +6,18 @@
 5.Install Firebase for your project: npm install firebase
 6.Dangerous :Get firebase config and put it in firebase.init.js
 7.export app from firebase.init.js
+----------------
+setup the provider
+------------------
 8.create auth using getAuth from firebase by using app from firebase.init.js
 9.create a google auth provider. do not forget to use new
 10.go to firebase > Build > authentication > sign in method
 11.enable google sign in method
+12.create a button for google sign in with method with a click handler
+13.inside the event handler ,call signinWithPopup with auth and provider
+14.after signInWithPopup
+-------------------
+Display Data
+-------------------
+
 */
